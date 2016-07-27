4c931e43333379b0339839fd3639b35d1d8a7067
mwright@event38.com <mwright@event38.com>
2016-07-27 14:22:35 -0400

    E386 Lidar Check
    Fixed a bug with E386 preflight checklist.

--- GCSViews/ConfigurationView/PreFlightChecklist.cs
+++ GCSViews/ConfigurationView/PreFlightChecklist.cs
@@ -21,61 +21,107 @@ namespace MissionPlanner.GCSViews.ConfigurationView
         {
 
 
-            //if ( MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.E386)
-            //{
-            //    if (MainV2.comPort.MAV.cs.satcount >= 5 && MainV2.comPort.MAV.cs.gpsstatus >= 3)
-            //    {
-            //        lblGPS.Image = MissionPlanner.Properties.Resources.Green_panel1;
-            //        chBoxGPS.Checked = true;
-            //        lblGPS.Text = "3D fix and 5 or more satellites connected";
-            //    }
-            //    else
-            //    {
-            //        lblGPS.Image = MissionPlanner.Properties.Resources.Red_panel;
-            //        chBoxGPS.Checked = false;
-            //        lblGPS.Text = "3D fix or satellites failed or both";
-            //    }
+            if (MainV2.CurrentUAV.firmware == "E386")
+            {
+                if (MainV2.CurrentUAV.firmware == "E386" && lidarchk.Visible == false)
+                {   
+                    
+                       lidarchk.Invoke(new Action(() =>
+                    {
+                lidarchk.Visible = true;
+                    }));
+                    LidarClean.Invoke(new Action(() =>
+                    {
+                        LidarClean.Visible = true;
+                    }));
+                    LidarCleanCHK.Invoke(new Action(() =>
+                    {
+                        LidarCleanCHK.Visible = true;
+                    }));
+                    LidarEnabled.Invoke(new Action(() =>
+                    {
+                        LidarEnabled.Visible = true;
+                    }));
+                    LidarText.Invoke(new Action(() =>
+                    {
+                        LidarText.Visible = true;
+                    }));
+                    YNlidar.Invoke(new Action(() =>
+                    {
+                        YNlidar.Visible = true;
+                    }));
+                    //LidarClean.Visible = true;
+                    //LidarCleanCHK.Visible = true;
+                    //LidarEnabled.Visible = true;
+                    //LidarText.Visible = true;
+                    //YNlidar.Visible = true;
 
-            //    if (MainV2.comPort.MAV.cs.linkqualitygcs >= 90)
-            //    {
-            //        lblTel.Image = MissionPlanner.Properties.Resources.Green_panel1;
-            //        lblTel.Text = "signal >= 90%";
-            //        chBoxTel.Checked = true;
-            //    }
-            //    else
-            //    {
-            //        lblTel.Image = MissionPlanner.Properties.Resources.Red_panel;
-            //        lblTel.Text = "signal < 90%";
-            //        chBoxTel.Checked = false;
-            //    }
+                }
+                if (MainV2.comPort.MAV.cs.satcount >= 5 && MainV2.comPort.MAV.cs.gpsstatus >= 3)
+                {
+                    lblGPS.Image = MissionPlanner.Properties.Resources.Green_panel1;
+                    chBoxGPS.Checked = true;
+                    lblGPS.Text = "3D fix and 5 or more satellites connected";
+                }
+                else
+                {
+                    lblGPS.Image = MissionPlanner.Properties.Resources.Red_panel;
+                    chBoxGPS.Checked = false;
+                    lblGPS.Text = "3D fix or satellites failed or both";
+                }
 
-            //    if (MainV2.comPort.MAV.cs.battery_voltage >= 15.99)
-            //    {
-            //        lblBattery.Text = "Voltage > 15.99";
-            //        lblBattery.Image = MissionPlanner.Properties.Resources.Green_panel1;
-            //        chBoxBattery.Checked = true;
-            //    }
-            //    else if (MainV2.comPort.MAV.cs.battery_voltage >= 14.5 && MainV2.comPort.MAV.cs.battery_voltage <= 15.98)
-            //    {
-            //        lblBattery.Text = "Voltage between 14.5 and 15.98";
-            //        lblBattery.Image = MissionPlanner.Properties.Resources.Yellow_panel;
-            //        chBoxBattery.Checked = true;
-            //    }
-            //    else
-            //    {
-            //        lblBattery.Text = "Voltage less than 14.5";
-            //        lblBattery.Image = MissionPlanner.Properties.Resources.Red_panel;
-            //        chBoxBattery.Checked = false;
-            //    }
-            //    this.label14.Text = "Valid Waypoints";
-            //    this.label3.Text = "All servos respond to tilting the aircraft";
-            //    this.label4.Text = "Center of gravity at points indicated ";
-            //    this.label2.Text = "All servos respond to R/C stick inputs";
-            //    this.label6.Text = "Servo linkages are secure";
-            //    this.label12.Text = "Tail and wings are secured to the body";
-            //    this.label5.Text = "Camera is on and test shot has been taken";
-            //    this.label15.Text = "Verify Compass Heading";
-            //}
+                if (MainV2.comPort.MAV.cs.linkqualitygcs >= 90)
+                {
+                    lblTel.Image = MissionPlanner.Properties.Resources.Green_panel1;
+                    lblTel.Text = "signal >= 90%";
+                    chBoxTel.Checked = true;
+                }
+                else
+                {
+                    lblTel.Image = MissionPlanner.Properties.Resources.Red_panel;
+                    lblTel.Text = "signal < 90%";
+                    chBoxTel.Checked = false;
+                }
+
+                if (MainV2.comPort.MAV.cs.battery_voltage >= 15.99)
+                {
+                    lblBattery.Text = "Voltage > 15.99";
+                    lblBattery.Image = MissionPlanner.Properties.Resources.Green_panel1;
+                    chBoxBattery.Checked = true;
+                }
+                else if (MainV2.comPort.MAV.cs.battery_voltage >= 14.5 && MainV2.comPort.MAV.cs.battery_voltage <= 15.98)
+                {
+                    lblBattery.Text = "Voltage between 14.5 and 15.98";
+                    lblBattery.Image = MissionPlanner.Properties.Resources.Yellow_panel;
+                    chBoxBattery.Checked = true;
+                }
+                else
+                {
+                    lblBattery.Text = "Voltage less than 14.5";
+                    lblBattery.Image = MissionPlanner.Properties.Resources.Red_panel;
+                    chBoxBattery.Checked = false;
+                }
+                if (MainV2.comPort.MAV.cs.sonarvoltage != 0)
+                {
+                    lidarchk.Checked = true;
+                    LidarText.Text = "Lidar is enabled";
+                    LidarText.Image = MissionPlanner.Properties.Resources.Green_panel;
+                }
+                else if (MainV2.comPort.MAV.cs.sonarvoltage == 0)
+                {
+                    lidarchk.Checked = false;
+                    LidarText.Text = "Check Lidar";
+                    LidarText.Image = MissionPlanner.Properties.Resources.Red_panel;
+                }
+                this.label14.Text = "Valid Waypoints";
+                this.label3.Text = "All servos respond to tilting the aircraft";
+                this.label4.Text = "Center of gravity at points indicated ";
+                this.label2.Text = "All servos respond to R/C stick inputs";
+                this.label6.Text = "Servo linkages are secure";
+                this.label12.Text = "Tail and wings are secured to the body";
+                this.label5.Text = "Camera is on and test shot has been taken";
+                this.label15.Text = "Verify Compass Heading";
+            }
 
             //if (MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.Scout)
             //{
@@ -132,8 +178,16 @@ namespace MissionPlanner.GCSViews.ConfigurationView
             //    this.label5.Text = "Camera is on and test shot has been taken";
             //    this.label15.Text = "Verify Compass Heading";
             //}
-            if (MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.ArduPlane || MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.Ateryx || MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.ArduRover || MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.ArduTracker)
+            if (MainV2.CurrentUAV.firmware == "E384")
             {
+                if (MainV2.CurrentUAV.firmware != "E386") {
+             lidarchk.Visible = false;
+             LidarClean.Visible = false;
+             LidarCleanCHK.Visible = false;
+             LidarEnabled.Visible = false;
+             LidarText.Visible = false;
+             YNlidar.Visible = false;
+                }
                 if (MainV2.comPort.MAV.cs.satcount >= 5 && MainV2.comPort.MAV.cs.gpsstatus >= 3)
                 {
                     lblGPS.Image = MissionPlanner.Properties.Resources.Green_panel1;
@@ -180,41 +234,15 @@ namespace MissionPlanner.GCSViews.ConfigurationView
                    lblBattery.Image = MissionPlanner.Properties.Resources.Red_panel;
                    chBoxBattery.Checked = false;
                 }
-        
-
-             
-                if (MainV2.comPort.MAV.cs.sonarvoltage != 0)
-                   {
-                    lidarchk.Checked = true;
-                    LidarText.Text = "Lidar is enabled";
-                    LidarText.Image = MissionPlanner.Properties.Resources.Green_panel;
-                   }
-              else if (MainV2.comPort.MAV.cs.sonarvoltage == 0)
-                   {
-                     lidarchk.Checked = false;
-                     LidarText.Text = "Check Lidar";
-                     LidarText.Image = MissionPlanner.Properties.Resources.Red_panel;
-                    }
 
 
 
-                if (MainV2.CurrentUAV.firmware == "E386")
-                {  
-                    lidarchk.Visible = true;
-                    LidarClean.Visible = true;
-                    LidarCleanCHK.Visible = true;
-                    LidarEnabled.Visible = true;
-                    LidarText.Visible = true;
-                    YNlidar.Visible = true;
-                }
-         if (MainV2.CurrentUAV.firmware != "E386") {
-             lidarchk.Visible = false;
-             LidarClean.Visible = false;
-             LidarCleanCHK.Visible = false;
-             LidarEnabled.Visible = false;
-             LidarText.Visible = false;
-             YNlidar.Visible = false;
-         }       
+                
+
+
+
+               
+                
                 this.label14.Text = "Valid Waypoints";
                 this.label3.Text = "All servos respond to tilting the aircraft";
                 this.label4.Text = "Center of gravity at points indicated ";
@@ -253,10 +281,11 @@ namespace MissionPlanner.GCSViews.ConfigurationView
          //   }
             //Need to add one more condition
 
-            if (MainV2.comPort.MAV.cs.firmware == MainV2.Firmwares.ArduCopter2)
+            if (MainV2.CurrentUAV.firmware == "Iris")
             {
                 if (MainV2.CurrentUAV.firmware != "E386")
                 {
+                    
                     lidarchk.Visible = false;
                     LidarClean.Visible = false;
                     LidarCleanCHK.Visible = false;
